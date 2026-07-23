381ed887c7948e7e1c15271752416f9953c13208
Bladetox <152655158+Bladetox@users.noreply.github.com>
2026-07-23 23:15:53 +0200

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -238,7 +238,7 @@ export const HeroSection = () => {
                 textDecoration: "none",
                 display: "inline-flex", alignItems: "center", gap: 8,
                 minHeight: 52, width: "auto",
-                justifyContent: isMobile ? "flex-start",
+                justifyContent: isMobile ? "center" : "flex-start",
               }}
             >
               Get clarity from R99
